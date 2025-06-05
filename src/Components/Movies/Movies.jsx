f8eeb24701e57d65d0f2347efa91f9f33cd6fcbb
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-05 12:07:55 +0300

    update fetching movies

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react'
-import useStyles from './Movies.style.js'
-import { Box, useMediaQuery, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB.js';
 import { FeaturedMovie, Loader, MovieList, Pagination } from './../index.js'
 
 
 export default function Movies() {
-    const classes = useStyles();
     const [page, setPage] = useState(1);
     const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.curruntGenreOrCategory);
     const { data, error, isLoading, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
 
-    const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
-    const numberOfMovies = lg ? 17 : 19;
-
-
-    if (isFetching) return <Loader size={'4rem'} />
+    if (isFetching || isLoading) return <Loader size={'4rem'} />
 
     if (!data?.results?.length) {
         return <Box display='flex' alignItems='center' mt='20px'>
@@ -32,7 +26,7 @@ export default function Movies() {
 
     return <>
         <FeaturedMovie movie={data?.results[0]} />
-        <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
+        <MovieList movies={data} numberOfMovies={data?.results?.length} excludeFirst />
         <Pagination curruntPage={page} setPage={setPage} totalPages={data?.total_pages} />
     </>
 }
