8a6c29fee7c77e89959faab59adfdb8bd100201d
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-05 16:40:16 +0300

    fix movie title width

--- src/Components/Movie/Movie.style.js
+++ src/Components/Movie/Movie.style.js
@@ -5,7 +5,7 @@ export default makeStyles((theme)=>({
     title: {
         color: theme.palette.text.primary,
         textOverflow: 'ellipsis',
-        width: '230px',
+        width: '100% !important',
         whiteSpace: 'nowrap',
         overflow: 'hidden',
         marginTop: '20px',
