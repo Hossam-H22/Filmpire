4cc3ac6c58acf2e5efafd8f99cc545fd025da138
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-05 12:04:21 +0300

    update movie style

--- src/Components/Movie/Movie.style.js
+++ src/Components/Movie/Movie.style.js
@@ -2,38 +2,27 @@
 import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme)=>({
-    movie: {
-        padding: '10px',
-    },
     title: {
         color: theme.palette.text.primary,
         textOverflow: 'ellipsis',
         width: '230px',
         whiteSpace: 'nowrap',
         overflow: 'hidden',
-        marginTop: '10px',
-        marginBottom: 0,
+        marginTop: '20px',
         textAlign: 'center',
     },
     links: {
         alignItems: 'center',
-        fontWeight: 'bolder',
-        textDecoration: 'none',
-        [theme.breakpoints.up('xs')]: {
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        '&:hover': {
-            cursor: 'pointer',
-        },
+        display: 'flex',
+        flexDirection: 'column',
+        cursor: 'pointer',
     },
     image: {
         borderRadius: '20px',
         height: '300px',
-        marginBottom: '10px',
         transition: 'all 0.2s',
         '&:hover': {
-            transform: 'scale(1.05)',
+            transform: 'scale(1.03)',
         },
     },
 }));
