21b13e3f1c1806f916384a57e951de2aeac92020
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-05 12:08:23 +0300

    update movies list

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import useStyles from './MovieList.style.js'
 import { Grid } from '@mui/material';
 import { Movie } from '..'
 
 export default function MovieList({ movies, numberOfMovies, excludeFirst }) {
-    const classes = useStyles();
-
     const startFrom = excludeFirst ? 1 : 0;
 
-    return <Grid container className={classes.moviesContainer}>
-        {movies?.results?.slice(startFrom, numberOfMovies)?.map((movie, index) => (
-            <Movie key={index} movie={movie} index={index} />
+    return <Grid container>
+        {movies?.results?.slice(startFrom, numberOfMovies)?.map((movie) => (
+            <Movie key={movie?.id} movie={movie} />
         ))}
     </Grid>
 }
