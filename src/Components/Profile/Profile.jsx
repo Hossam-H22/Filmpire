93e74c7fa4e946053208de3ea558bb3537a19675
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-05 18:46:32 +0300

    hotFix/profile-padding

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import useStyles from './Profile.style.js'
 import { useSelector } from 'react-redux';
 import { userSelector } from '../../features/auth.js';
 import { Box, Button, Typography } from '@mui/material';
@@ -10,42 +9,36 @@ import { Helmet } from 'react-helmet';
 
 
 export default function Profile() {
-    const classes = useStyles();
-    
     const { user } = useSelector(userSelector);
     const sessionId = localStorage.getItem('session_id');
     const [favoritePage, setFavoritePage] = useState(1);
     const [watchlistPage, setWatchlistPage] = useState(1);
-    const { data: favoriteMovies, 
-        isFetching: isFetchingFavoriteMovies, 
-        refetch: refetchFavorites } = useGetListQuery({listName: 'favorite/movies', accountId: user.id, sessionId: sessionId, page: favoritePage});
+    const { data: favoriteMovies,
+        isFetching: isFetchingFavoriteMovies,
+        refetch: refetchFavorites } = useGetListQuery({ listName: 'favorite/movies', accountId: user.id, sessionId: sessionId, page: favoritePage });
 
-    const { data: watchlistMovies, 
-        isFetching: isFetchingWatchlistMovies, 
-        refetch: refetchWatchlistes } = useGetListQuery({listName: 'watchlist/movies', accountId: user.id, sessionId: sessionId, page: watchlistPage});
+    const { data: watchlistMovies,
+        isFetching: isFetchingWatchlistMovies,
+        refetch: refetchWatchlistes } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: sessionId, page: watchlistPage });
 
-    console.log(watchlistMovies);
-
-
-    useEffect(()=>{
+    useEffect(() => {
         refetchFavorites();
         refetchWatchlistes();
     }, []);
 
 
-    
     function logout() {
         localStorage.clear();
         window.location.href = '/';
     }
 
-    if(isFetchingFavoriteMovies || isFetchingWatchlistMovies) return <Loader size='8rem' />
+    if (isFetchingFavoriteMovies || isFetchingWatchlistMovies) return <Loader size='8rem' />
 
     return <>
         <Helmet>
             <title>Profile</title>
         </Helmet>
-        <Box>
+        <Box sx={{ padding: '20px' }} >
             <Box display='flex' justifyContent='space-between' >
                 <Typography variant='h4' gutterBottom>My Profie</Typography>
                 <Button color='inherit' onClick={logout}>
@@ -53,23 +46,23 @@ export default function Profile() {
                 </Button>
             </Box>
 
-            {!favoriteMovies?.total_results && !watchlistMovies?.total_results ? 
+            {!favoriteMovies?.total_results && !watchlistMovies?.total_results ?
                 <Typography variant='h5' >Add favorite or watchlist some movies to see them here!</Typography>
-                : 
+                :
                 <Box>
-                    {favoriteMovies?.total_results && <RatedCards 
-                        title='Favorite Movies' 
-                        data={favoriteMovies} 
-                        page={favoritePage} 
+                    {favoriteMovies?.total_results && <RatedCards
+                        title='Favorite Movies'
+                        data={favoriteMovies}
+                        page={favoritePage}
                         setPage={setFavoritePage}
                     />}
-                    {watchlistMovies?.total_results && <RatedCards 
-                        title='Watchlist' 
+                    {watchlistMovies?.total_results && <RatedCards
+                        title='Watchlist'
                         data={watchlistMovies}
-                        page={watchlistPage} 
+                        page={watchlistPage}
                         setPage={setWatchlistPage}
                     />}
-                </Box> 
+                </Box>
             }
         </Box>
     </>
