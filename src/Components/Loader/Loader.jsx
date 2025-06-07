f42aed3013f142b0d35368ebdccf55c6e1556472
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-07 16:12:49 +0300

    update loader component

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Box, CircularProgress } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
-export default function Loader({ size }) {
+export default function Loader({ size, removeMargin }) {
     const theme = useTheme();
 
-    return <Box display='flex' justifyContent='center' mt={2} >
+    return <Box display='flex' justifyContent='center' mt={removeMargin ? 0 : 2} >
         <CircularProgress size={size ?? '2rem'} sx={{ color: theme.palette.mode === 'dark' ? '#fff': theme.palette.primary }} />
     </Box>
 }
