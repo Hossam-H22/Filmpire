05799562a82cac37488dd8c3f1b153d2ca646e3b
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-07 11:11:02 +0300

    create new component for actor card

---
+++ src/Components/ActorCard/ActorCard.style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles((theme) => ({
+    castImage: {
+        width: '100%',
+        maxWidth: '7em',
+        height: '8em',
+        objectFit: 'cover',
+        borderRadius: '10px',
+    },
+}));
