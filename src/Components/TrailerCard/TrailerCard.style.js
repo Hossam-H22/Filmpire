cbf0c3490396d250457d41175353efb7aaff6032
Hossam-H22 <hosam.hatem222@gmail.com>
2025-06-07 16:09:42 +0300

    update trailer card

---
+++ src/Components/TrailerCard/TrailerCard.style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles((theme) => ({
+    Loader: {
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 1000,
+    },
+}));
